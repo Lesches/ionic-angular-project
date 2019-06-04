99168b4eab99bd83931a2a21f145b981db5dee21
sholom <sholomls@gmail.com>
2019-06-03 23:01:47 -0400

    fixed the places component routing

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -6,22 +6,24 @@ const routes: Routes = [
     {path: 'tabs', component: PlacesPage, children: [
             {path: 'discover', children: [
                     {path: '', loadChildren: './discover/discover.module#DiscoverPageModule'},
-                    {path: ':placeId', loadChildren: './discover/place-detail/place-detail.module#PlaceDetailPageModule'},
+                    {path: ':placeId', loadChildren: './discover/place-detail/place-detail.module#PlaceDetailPageModule'}
 
                 ]},
-            {path: '', redirectTo: '/places/tabs/discover', pathMatch: 'full'}
+            {path: '', redirectTo: '/places/tabs/discover', pathMatch: 'full'},
+            {path: 'offers', children: [{
+                    path: '', loadChildren: './offers/offers.module#OffersPageModule'},
+                    {path: 'new', loadChildren: './offers/new-offer/new-offer.module#NewOfferPageModule'},
+                    {path: 'edit/:placeId', loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'},
+                    {path: ':placeId', loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'},
+
+                ]},
         ]
     },
-    {path: 'offers', children: [{
-        path: '', loadChildren: './offers/offers.module#OffersPageModule'},
-            {path: 'new', loadChildren: './offers/new-offer/new-offer.module#NewOfferPageModule'},
-            {path: 'edit/:placeId', loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'},
-            {path: ':placeId', loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'}
-            ]},
+
     {path: '', redirectTo: '/places/tabs/discover', pathMatch: 'full'}
 ];
 
-NgModule({
+@NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
