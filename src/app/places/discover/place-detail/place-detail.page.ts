7d4c07312f14c767ab0e5ef80d512ac5e4d4cfc1
sholom <sholomls@gmail.com>
2019-06-13 11:33:49 -0400

    create booking

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import {NavController} from '@ionic/angular';
+import {ModalController, NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -9,13 +9,12 @@ import {NavController} from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  constructor(private router: Router, private navCtrl: NavController, private modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
   onBookPlace() {
-    this.navCtrl.navigateBack('/places/tabs/discover');
-   // this.navCtrl.pop();
+    this.modalCtrl.create()
   }
 
 }
