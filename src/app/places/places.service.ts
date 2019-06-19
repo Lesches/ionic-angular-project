feecffb2bfa0ef4dc9a41505636854df235725c0
sholom <sholomls@gmail.com>
2019-06-19 14:40:33 -0400

    to Isos string

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -6,9 +6,9 @@ import {Place} from './place.model';
 })
 export class PlacesService {
   private place: Place[] = [
-     new Place('p1', 'Manhattan Mansion', 'in the heart of NYC', 'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042534/Felix_Warburg_Mansion_007.jpg', 149.99),
-    new Place('p2', 'L\'Amour Toujours', 'Romantic place in Paris', 'https://www.sheetmusicwarehouse.co.uk/image/w555/6681/love-everlasting-lamour-toujours-lamour-song-in-the-key-of-c-major.jpg', 300.99),
-    new Place('p3', 'Trump Tower', 'Hotel in Manhattan', 'https://ichef.bbci.co.uk/news/660/cpsprodpb/0E14/production/_105240630_gettyimages-1080368310.jpg', 600.99)
+     new Place('p1', 'Manhattan Mansion', 'in the heart of NYC', 'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042534/Felix_Warburg_Mansion_007.jpg', 149.99, new Date('2019-1-1'), new Date('2019-1-2')),
+    new Place('p2', 'L\'Amour Toujours', 'Romantic place in Paris', 'https://www.sheetmusicwarehouse.co.uk/image/w555/6681/love-everlasting-lamour-toujours-lamour-song-in-the-key-of-c-major.jpg', 300.99, new Date('2019-1-1'), new Date('2019-1-2')),
+    new Place('p3', 'Trump Tower', 'Hotel in Manhattan', 'https://ichef.bbci.co.uk/news/660/cpsprodpb/0E14/production/_105240630_gettyimages-1080368310.jpg', 600.99, new Date('2019-1-1'), new Date('2019-1-2'))
   ];
 
   get places() {
