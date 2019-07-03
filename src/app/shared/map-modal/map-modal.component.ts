13c6313b37f60eab0b294f200f030bbe2ba39a94
sholom <sholomls@gmail.com>
2019-07-03 13:41:23 -0400

    reusing the maps modal

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, Renderer2, OnDestroy} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, Renderer2, OnDestroy, Input} from '@angular/core';
 import {ModalController} from '@ionic/angular';
 import {environment} from '../../../environments/environment';
 
@@ -9,6 +9,10 @@ import {environment} from '../../../environments/environment';
 })
 export class MapModalComponent implements OnInit, AfterViewInit, OnDestroy{
 @ViewChild('map') mapElementRef: ElementRef;
+@Input() center = {lat: -34.397, lng: 150.644};
+@Input() selectable = true;
+@Input() closeButtonText = 'Cancel';
+@Input() title = 'Pick Location';
 clickListener: any;
 googleMaps: any;
   constructor(private modalCtrl: ModalController, private renderer: Renderer2) { }
@@ -20,7 +24,7 @@ googleMaps: any;
       this.googleMaps = googleMaps;
       const mapEl = this.mapElementRef.nativeElement;
       const map = new googleMaps.Map(mapEl, {
-  center: {lat: -34.397, lng: 150.644},
+  center: this.center,
   zoom: 16
 });
 
@@ -28,10 +32,18 @@ googleMaps: any;
   this.renderer.addClass(mapEl, 'visible');
   });
 
-      this.clickListener = map.addListener('click', event => {
-  const selectedCoords = {lat: event.latLng.lat() , lng: event.latLng.lng()};
-  this.modalCtrl.dismiss(selectedCoords);
-});
+
+      if (this.selectable) {
+        this.clickListener = map.addListener('click', event => {
+          const selectedCoords = {lat: event.latLng.lat(), lng: event.latLng.lng()};
+          this.modalCtrl.dismiss(selectedCoords);
+        });
+      } else {
+        const marker = new googleMaps.Marker({
+          position: this.center, map, title: 'Picked Location'
+        });
+        marker.setMap(map);
+      }
     }).catch( err => {
       console.log(err);
     });
@@ -42,7 +54,9 @@ googleMaps: any;
   }
 
   ngOnDestroy() {
-this.googleMaps.event.removeListener(this.clickListener);
+    if (this.clickListener) {
+      this.googleMaps.event.removeListener(this.clickListener);
+    }
   }
 
   private getGoogleMaps(): Promise<any> {
