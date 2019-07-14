99c4c10ddb6697b4a2de6e2c8cd4d8c01bdb9312
sholom <sholomls@gmail.com>
2019-07-14 13:21:02 -0400

    adding autologin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, from} from 'rxjs';
 import {User} from './user.model';
 import {map, tap} from 'rxjs/operators';
 import {Plugins} from '@capacitor/core';
@@ -39,6 +39,28 @@ get UserId() {
 }
   constructor(private http: HttpClient) { }
 
+  autoLogin() {
+  return from(Plugins.Storage.get({key: 'authData'})).pipe(map(storedData => {
+    if (!storedData || !storedData.value) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(storedData.value) as {token: string; tokenExpirationDate: string; userId: string, email: string};
+    const expirationTime = new Date(parsedData.tokenExpirationDate);
+    if (expirationTime <= new Date()) {
+      return null;
+    }
+    const user = new User(parsedData.userId, parsedData.email, parsedData.token, expirationTime);
+    return user;
+  }),
+      tap(user =>  {
+if (user) {
+  this.user.next(user);
+}
+    }), map(user => {
+      return !!user;
+      }));
+}
   signup(email: string, password: string) {
 return this.http.post<AuthResponseData>(
     `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${environment.firebaseAPIKey}`,
@@ -57,11 +79,11 @@ return this.http.post<AuthResponseData>(
 private setUserData(userData: AuthResponseData) {
   const expirationTime = new Date(new Date().getTime() + (+userData.expiresIn * 1000));
   this.user.next(new User(userData.localId, userData.email, userData.idToken, expirationTime));
-  this.storeAuthData(userData.localId, userData.idToken, expirationTime.toISOString());
+  this.storeAuthData(userData.localId, userData.idToken, expirationTime.toISOString(), userData.email);
 }
 
-private storeAuthData(userId: string, token: string, tokenExpirationDate: string) {
-  const data = JSON.stringify({userId, token, tokenExpirationDate});
+private storeAuthData(userId: string, token: string, tokenExpirationDate: string, email: string) {
+  const data = JSON.stringify({userId, token, tokenExpirationDate, email});
 
   Plugins.Storage.set({key: 'authData', value: data});
 }
