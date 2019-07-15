f89ff6158a27731b6e40ecf831308540a1acbd28
sholom <sholomls@gmail.com>
2019-07-15 18:57:29 -0400

    more token usage

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -36,14 +36,21 @@ constructor(private authService: AuthService, private http: HttpClient) {
              dateFrom: Date, dateTo: Date) {
   let generatedId: string;
   let newBooking: Booking;
+  let fetchedUserId: string;
   return this.authService.UserId.pipe(take(1), switchMap(userId => {
-    if (!userId) {
-      throw new Error('No user id found!');
-    }
-    newBooking = new Booking(Math.random().toString(), placeId, userId,
-        placeTitle, placeImage, fname, lname, guestNumber, dateFrom, dateTo);
-    return this.http.post<{name: string}>('https://maga-da45c.firebaseio.com/bookings.json',
-        {...newBooking, id: null}); }),
+      fetchedUserId = userId;
+      if (!userId) {
+          throw new Error('No user id found!');
+      }
+      fetchedUserId = userId;
+      return this.authService.Token;
+  }),
+      take(1), switchMap(token => {
+          newBooking = new Booking(Math.random().toString(), placeId, fetchedUserId,
+              placeTitle, placeImage, fname, lname, guestNumber, dateFrom, dateTo);
+          return this.http.post<{name: string}>(
+              `https://maga-da45c.firebaseio.com/bookings.json?auth=${token}`,
+              {...newBooking, id: null}); }),
       switchMap(resData => {
           generatedId = resData.name;
           return this.bookings;
@@ -55,20 +62,30 @@ constructor(private authService: AuthService, private http: HttpClient) {
   }
 
   cancelBooking(bookingId: string) {
-  return this.http.delete(`https://maga-da45c.firebaseio.com/bookings/${bookingId}.json`).pipe(switchMap(() => {
+    return this.authService.Token.pipe(take(1), switchMap(token => {
+        return this.http.delete(`https://maga-da45c.firebaseio.com/bookings/${bookingId}.json?auth=${token}`);
+    }), switchMap(() => {
 return this.bookings;
     }), take(1), tap(bookings => {
     this.bookings.next(bookings.filter(b => b.id !== bookingId));
   }));
   }
+
 fetchBookings() {
- return this.authService.UserId.pipe(take(1), switchMap(userId => {
-    if (!userId) {
-      throw new Error('User not found!');
-    }
-    return this.http.get<{[key: string]: BookingData}>(
-        `https://maga-da45c.firebaseio.com/bookings.json?ordereBy="UserId"&equalTo="${this.authService.UserId}"`)
-        .pipe(map(bookingData => {
+    let fetchedUserId: string;
+    return this.authService.UserId.pipe(take(1), switchMap(userId => {
+     if (!userId) {
+         throw new Error('User not found!');
+     }
+     fetchedUserId = userId;
+     return this.authService.Token;
+ }),
+    take(1),
+        switchMap(token => {
+            return this.http.get<{ [key: string]: BookingData }>(
+                `https://maga-da45c.firebaseio.com/bookings.json?ordereBy="UserId"&equalTo="${fetchedUserId}"&auth=${token}`);
+        }),
+        map(bookingData => {
           const bookings = [];
           for (const key in bookingData) {
             if (bookingData.hasOwnProperty(key)) {
@@ -82,6 +99,5 @@ fetchBookings() {
           this.bookings.next(bookings);
         }));
       }
-  ));
 }
-}
+
